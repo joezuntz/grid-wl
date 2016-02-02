ce053ac1d816565a449ec3ccea617d79e4cdcf19
Joe Zuntz <joezuntz@googlemail.com>
2016-02-02 15:38:34 +0000

    newest version

--- launch_and_run.sh
+++ launch_and_run.sh
@@ -4,82 +4,59 @@
 set -o xtrace
 
 #Parameters of this run
-STORAGE_ELEMENT=srm://bohr3226.tier2.hep.manchester.ac.uk/dpm/tier2.hep.manchester.ac.uk/home/lsst/zuntz
-CODE_DIR=/cvmfs/lsst.opensciencegrid.org/uk/shape-measurement/im3shape/im3shape-2015-08-03
-INPUT_BASE=meds
-OUTPUT_BASE=results
-RUN_NUMBER=1
+CODE_REMOTE_PATH=$1
+DATA_REMOTE_PATH=$2
+OUTPUT_REMOTE_PATH=$3
+INI=$4
+CAT=$5
+JOB_RANK=$6
+JOB_COUNT=$7
+BLACKLIST=$8
 
-#should be distributed with the code
-DATASET=$1
+CODE_LOCAL_PATH=$(basename $CODE_REMOTE_PATH)
+DATA_LOCAL_PATH=$(basename $DATA_REMOTE_PATH)
+OUTPUT_LOCAL_PATH=$(basename $OUTPUT_REMOTE_PATH)
 
-#Input files, need to be brought from somewhere
-MEDS=$2
-CAT=$3
+echo CODE_LOCAL_PATH $CODE_LOCAL_PATH
+echo DATA_LOCAL_PATH $DATA_LOCAL_PATH
+echo OUTPUT_LOCAL_PATH $OUTPUT_LOCAL_PATH
 
-#Output file base, $OUT.main.txt and $OUT.epoch.txt need
-#to be brought back after run
-OUT=$4
-
-#Which part of this file to run.  FIRST is now interpreted as a job index
-#our of JOB_COUNT
-FIRST=$5
-JOB_COUNT=$6
-
-INI=$7
-
-#Dummy value since we are using --split below
-COUNT=1
-
-
-INDIR=$STORAGE_ELEMENT/$INPUT_BASE/$DATASET/
-OUTDIR=$STORAGE_ELEMENT/$OUTPUT_BASE/v$RUN_NUMBER/$DATASET/
-
+# Download the software to be run, unzip it, and go into the directory
+echo Downloading Code `date`
+gfal-copy $CODE_REMOTE_PATH file://$PWD/$CODE_LOCAL_PATH  2>&1
+tar -xf $CODE_LOCAL_PATH  2>&1
+cd im3shape-grid 2>&1
+pwd
 echo Downloading data `date`
-#Download/obtain data $MEDS and $CAT somehow
-gfal-copy $INDIR/$MEDS file://$PWD/$MEDS 2>&1
+gfal-copy $DATA_REMOTE_PATH file://$PWD/$DATA_LOCAL_PATH 2>&1
+
+#Move files into dir. For convenience
+mv ../$INI ./
+mv ../$BLACKLIST ./
 
 #Run the main code - also sets up environment
 echo Running code `date`
-
-#Some of the im3shape variables interfere with the gfal commands, so we will save and restore.
-#We should put this in a packaged run_im3shape  script but I want to test it now and there is a three hour
-#delay when you update CVFMFS
-
-OLD_PATH=$PATH
-OLD_LD=$LD
-
-
-pwd
-#Set up im3shape - paths etc
-pushd $CODE_DIR
-. setup-im3shape
-popd
-
-pwd
-ls $CODE_DIR
-ls $INI
-cat $INI
-
-#Actually tun the code
-python -m py3shape.analyze_meds2 $MEDS $INI $CAT $OUT $FIRST $COUNT --split $JOB_COUNT 2>&1
-
-#Restore old paths so we can run gfal-copy
-export PATH=$OLD_PATH
-export LD_LIBRARY_PATH=$OLD_LD
-unset PYTHON_LIB
-unset PYTHON_VERSION
-unset PYTHONPATH
-unset PYTHONHOME
-unset PYTHON_INCLUDE
-unset PYTHON_DIR
-unset PYTHON_ROOT
+ls
+echo ./run-im3shape  $DATA_LOCAL_PATH $INI $CAT $OUTPUT_LOCAL_PATH $JOB_RANK $JOB_COUNT  2>&1
+./run-im3shape  $DATA_LOCAL_PATH $INI $CAT $OUTPUT_LOCAL_PATH $JOB_RANK $JOB_COUNT  2>&1
+echo "DONE - WHAT IS HERE"
+ls
 
 
 echo Copying back results  `date`
-# Copy results home somehow
-gfal-mkdir -p $OUTDIR 2>&1
-gfal-copy file://$PWD/output.main.txt $OUTDIR/$MEDS.main.$FIRST.txt 2>&1
-gfal-copy file://$PWD/output.epoch.txt $OUTDIR/$MEDS.epoch.$FIRST.txt 2>&1
+# Copy results back to SRM. Force copying so we overwrite
+gfal-copy -f file://$PWD/${OUTPUT_LOCAL_PATH}.main.txt ${OUTPUT_REMOTE_PATH}.main.txt  2>&1
+gfal-copy -f file://$PWD/${OUTPUT_LOCAL_PATH}.epoch.txt ${OUTPUT_REMOTE_PATH}.epoch.txt  2>&1
 
 echo Complete  `date`
+
+
+
+
+#STORAGE_ELEMENT=srm://bohr3226.tier2.hep.manchester.ac.uk/dpm/tier2.hep.manchester.ac.uk/home/lsst/zuntz
+#CODE_REMOTE_PATH=srm://gridpp09.ecdf.ed.ac.uk:8446/srm/managerv2?SFN=/dpm/ecdf.ed.ac.uk/home/gridpp/lsst
+#CODE_DIR=/cvmfs/lsst.opensciencegrid.org/uk/shape-measurement/im3shape/im3shape-2015-08-03
+#INPUT_BASE=meds
+#OUTPUT_BASE=results
+#RUN_NUMBER=1
+
---
+++ meds3.py
@@ -0,0 +1,145 @@
+import Ganga.GPI
+import itertools
+import os
+import re
+import sys
+import collections
+
+
+class MedsJobs(object):
+    code_root='srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/gridpp/lsst/zuntz'
+    output_root=code_root #'srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/gridpp/lsst/zuntz'
+    data_root='srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/lsst'
+    def __init__(self, run_name, code_date, blacklist_file='blacklist-y1.txt', ini='params_bd.ini', nsplit=100, debug=0, local=False):
+        self.run_name = run_name
+        self.code_date = code_date
+        self.tree = Ganga.GPI.jobtree
+        self.blacklist_file=blacklist_file
+        self.ini_file = ini
+        self.debug=debug
+        self.local=local
+        self.nsplit=nsplit
+        self.root='/'+run_name
+        self.tree.mkdir(self.root)
+
+    def add_meds(self, tile_file): #e.g. tile_file='DES0001-4914-z-meds-y1a1-beta.fits.fz'
+        run_name=self.run_name #e.g. "y1a1-v2-z"
+        code_date=self.code_date #e.g. "2016-02-01"
+        output_root=self.output_root
+        code_root=self.code_root
+        data_root=self.data_root
+        split_count=self.nsplit
+        ini=self.ini_file
+        cat='all'
+        import os
+        tile_name = os.path.basename(tile_file)
+        job_name='{0}.{1}.{2}'.format(run_name, code_date, tile_name)
+        
+        code_remote_path="{code_root}/{run_name}/software/{code_date}/im3shape-grid.tar.gz".format(**locals())        
+        data_remote_path="{data_root}/{tile_file}".format(**locals())
+        
+        
+        
+
+        #The nsplit parameter controls two things:
+        #  - the number of jobs we actually create
+        #  - the number of jobs im3shape thinks there are 
+        #    (which determines the number of galaxies run in each job)
+        # The debug argument lets you cut the former down
+        if self.debug:
+            job_count = self.debug
+        else:
+            job_count = split_count
+
+        # The arguments, a list, one for each sub-job
+        #The list is of lists, containing the arguments to each subjob.
+        arg_sets=[
+            [code_remote_path, data_remote_path, 
+             "{output_root}/{run_name}/results/{tile_file}.{job_rank}.{split_count}".format(**locals()),  #output path difference for each subjob
+             ini, cat, job_rank, split_count,
+             self.blacklist_file,
+            ] 
+            for job_rank in xrange(job_count)
+        ]
+
+        splitter=Ganga.GPI.ArgSplitter(args=arg_sets)
+        exe=Ganga.GPI.Executable(exe=Ganga.GPI.File("launch_and_run.sh"))
+        backend=self.get_backend()
+
+        job=Ganga.GPI.Job(application=exe, backend=backend, name=job_name, splitter=splitter)
+        job.inputfiles = [Ganga.GPI.LocalFile(self.ini_file), Ganga.GPI.LocalFile(self.blacklist_file)]
+
+        self.tree.add(job, self.root)
+
+    def get_backend(self):
+        if self.local:
+            backend=Ganga.GPI.Local()
+        else:
+            backend=Ganga.GPI.CREAM()
+            backend.CE = self.get_cream()
+        return backend
+
+    def get_cream(self):
+        #alter this to do dynamic selection
+        return 'grid002.jet.efda.org:8443/cream-pbs-gridpp'
+            
+    def all_jobs(self):
+        jobs = self.tree.getjobs(self.root)
+        return jobs
+    
+    def submit(self):
+        for job in self.all_jobs():
+            job.submit()
+
+    def add_list(self, meds_list):
+        meds_files = [line.strip() for line in open(meds_list) if not line.strip().startswith('#')]
+        for meds_file in meds_files:
+            self.add_meds(meds_file)
+
+    def status_histogram(self):
+        jobs = flatten_jobs(self.all_jobs())
+        job_histogram_by_ce(jobs)
+        
+
+def flatten_jobs(jobs):
+    js = []
+    for job in jobs:
+        if job.subjobs:
+            js+=job.subjobs
+        else:
+            js.append(job)
+    return js
+
+def job_histogram_by_ce(jobs):
+    key = lambda j: j.backend.CE
+    jobs = flatten_jobs(jobs)
+    jobs = sorted(jobs, key=key)
+    for ce, js in itertools.groupby(jobs, key):
+        print ce
+        job_histogram(js)
+        print
+
+    
+
+def job_histogram(jobs):
+    js = flatten_jobs(jobs)
+    counts = collections.defaultdict(int)
+    for j in js:
+        counts[j.status]+=1
+    total = sum(counts.values())
+    for status, count in counts.items():
+        print '%.1f%% %s' % (count*100.0/total, status)
+
+def test():
+    run_name="y1a1-v2-z"
+    tile_file='DES2356-4831-z-meds-y1a1-alpha.fits.fz'
+    code_date="2016-02-01" #for file path
+    debug=5 #actual number of jobs created
+    fn='DES0454-4331-r-meds-spt-e-gold.fits.fz'
+    j=MedsJobs(run_name, code_date, local=False, debug=debug)
+    j.add_meds(tile_file)
+    return j
+
+if __name__=="__main__":
+    j = test()
+#    j.submit()
