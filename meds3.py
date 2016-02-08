f3cfdfa14fd1d01be43d067859f4844f08175193
Joe Zuntz <joezuntz@googlemail.com>
2016-02-08 15:29:45 +0000

    update for new cream and naming setup

--- meds3.py
+++ meds3.py
@@ -4,13 +4,16 @@ import os
 import re
 import sys
 import collections
+import itertools
 
+creams = [line.strip() for line in open('working_ces.txt').readlines() if line.strip()]
+cream_cycle = itertools.cycle(creams)
 
 class MedsJobs(object):
     code_root='srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/gridpp/lsst/zuntz'
     output_root=code_root #'srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/gridpp/lsst/zuntz'
     data_root='srm://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/lsst'
-    def __init__(self, run_name, code_date, blacklist_file='blacklist-y1.txt', ini='params_bd.ini', nsplit=100, debug=0, local=False):
+    def __init__(self, tree_name, run_name, code_date, blacklist_file='blacklist-y1.txt', ini='params_bd.ini', nsplit=20, debug=0, local=False):
         self.run_name = run_name
         self.code_date = code_date
         self.tree = Ganga.GPI.jobtree
@@ -19,7 +22,7 @@ class MedsJobs(object):
         self.debug=debug
         self.local=local
         self.nsplit=nsplit
-        self.root='/'+run_name
+        self.root='/'+tree_name
         self.tree.mkdir(self.root)
 
     def add_meds(self, tile_file): #e.g. tile_file='DES0001-4914-z-meds-y1a1-beta.fits.fz'
@@ -80,8 +83,9 @@ class MedsJobs(object):
         return backend
 
     def get_cream(self):
+        return cream_cycle.next()
         #alter this to do dynamic selection
-        return 'grid002.jet.efda.org:8443/cream-pbs-gridpp'
+#        return 'grid002.jet.efda.org:8443/cream-pbs-gridpp'
             
     def all_jobs(self):
         jobs = self.tree.getjobs(self.root)
@@ -130,15 +134,27 @@ def job_histogram(jobs):
     for status, count in counts.items():
         print '%.1f%% %s' % (count*100.0/total, status)
 
-def test():
+def test_one():
     run_name="y1a1-v2-z"
+    tree_name='run1'
     tile_file='DES2356-4831-z-meds-y1a1-alpha.fits.fz'
     code_date="2016-02-01" #for file path
     debug=5 #actual number of jobs created
-    fn='DES0454-4331-r-meds-spt-e-gold.fits.fz'
-    j=MedsJobs(run_name, code_date, local=False, debug=debug)
+    j=MedsJobs(tree_name, run_name, code_date, local=False, debug=debug)
     j.add_meds(tile_file)
     return j
+
+def test_33():
+    tree_name='run33'
+    run_name="y1a1-v2-z"
+    tile_file='DES2356-4831-z-meds-y1a1-alpha.fits.fz'
+    code_date="2016-02-01" #for file path                                                                                                                                          
+    j=MedsJobs(tree_name, run_name, code_date, local=False)
+    tile_files = [line.strip() for line in open('meds-y1.txt').readlines() if line.strip()]
+    tile_files = tile_files[:33]
+    for tile_file in tile_files:
+        j.add_meds(tile_file)
+
 
 if __name__=="__main__":
     j = test()
